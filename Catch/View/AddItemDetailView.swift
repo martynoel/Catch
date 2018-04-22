3809ce719a58cfb24fbc4b242eaa475c7441afc8
Mimi Chenyao <mimichenyao@gmail.com>
2018-04-22 05:55:33 -0400

    Added AddItemView and ItemDetailView to keep code modular

---
+++ Catch/View/AddItemDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  AddItemDetailView.swift
+//  Catch
+//
+//  Created by Mimi Chenyao on 4/22/18.
+//  Copyright © 2018 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
---
+++ Catch/View/ItemDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  ItemDetailView.swift
+//  Catch
+//
+//  Created by Mimi Chenyao on 4/22/18.
+//  Copyright © 2018 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
